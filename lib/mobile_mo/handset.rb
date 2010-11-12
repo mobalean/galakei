a5af8179b14244ae48d6a6fa0a20b2ec3470b851
Paul McMahon <paul@mobalean.com>
2010-11-12 12:48:29 +0900

    Add au and docomo

--- lib/mobile_mo/handset.rb
+++ lib/mobile_mo/handset.rb
@@ -11,5 +11,14 @@ module MobileMo
     def docomo?
       /DoCoMo/i =~ @request.user_agent
     end
+
+    def au?
+      # doesn't detect some 2G phones, but as they will be discontinued soon, doesn't really matter
+      /KDDI/ =~ @request.user_agent
+    end
+
+    def softbank?
+      /^(SoftBank|Vodafone)/ =~ @request.user_agent
+    end
   end
 end
--- test/handset_test.rb
+++ test/handset_test.rb
@@ -19,6 +19,17 @@ class HandsetTest < Test::Unit::TestCase
     assert handset('DoCoMo/1.0').docomo?
   end
 
+  def test_au
+    assert !handset('Mozilla').au?
+    assert handset('KDDI-SH32 UP.Browser/6.2.0.11.2.1 (GUI) MMP/2.0').au?
+  end
+
+  def test_softbank
+    assert !handset('Mozilla').softbank?
+    assert handset('Vodafone/1.0/V903SH/SHJ001').softbank?
+    assert handset('SoftBank/1.0/812SHs/SHJ001').softbank?
+  end
+
   private
 
   def handset(user_agent)
