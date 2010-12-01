9abb26a33f81754108485dea246af271d83220b3
Michael Reinsch <michael@mobalean.com>
2010-12-01 20:13:04 +0900

    seperated out method to detect whether or not session parm is used or not

--- lib/mobile_mo/session_id_parameter.rb
+++ lib/mobile_mo/session_id_parameter.rb
@@ -6,8 +6,12 @@ module MobileMo::SessionIdParameter
 
   protected # the following methods are available within the controller
 
+  def device_needs_session_param_in_url?
+    !handset.cookies? && session
+  end
+
   def set_session_id_parameter
-    if !handset.cookies? && session
+    if device_needs_session_param_in_url?
       session[:_dummy_param_to_force_session_init] = nil
       key = Rails.application.config.session_options[:key]
       logger.debug("adding session param '#{key}' to default_url_options")
--- mobile_mo.gemspec
+++ mobile_mo.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name         = "mobile_mo"
-  s.version      = '0.2.1'
+  s.version      = '0.2.2'
   s.platform     = Gem::Platform::RUBY
   s.authors      = ["Paul McMahon", "Michael Reinsch"]
   s.email        = "info@mobalean.com"
