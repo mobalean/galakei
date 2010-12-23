c671785572aea2d4defd9987c5f3a1d6f9d47afa
Paul McMahon <paul@mobalean.com>
2010-12-23 07:11:39 -0800

    Use autoload

--- lib/mobile_mo.rb
+++ lib/mobile_mo.rb
@@ -1,8 +1,11 @@
-module MobileMo; end
-
-require "mobile_mo/action_controller/base"
-require "mobile_mo/action_controller/haml"
 require "mobile_mo/railtie"
-require "mobile_mo/request"
-require "mobile_mo/email"
-require "mobile_mo/action_controller/session_id_parameter"
+
+module MobileMo
+  autoload :Request, "mobile_mo/request"
+  autoload :Email, "mobile_mo/email"
+  module ActionController
+    autoload :Base, "mobile_mo/action_controller/base"
+    autoload :Haml, "mobile_mo/action_controller/haml"
+    autoload :SessionIdParameter, "mobile_mo/action_controller/session_id_parameter"
+  end
+end
--- lib/mobile_mo/railtie.rb
+++ lib/mobile_mo/railtie.rb
@@ -1,14 +1,16 @@
-class MobileMo::Railtie < Rails::Railtie
-  config.mobile_mo = ActiveSupport::OrderedOptions.new
-  config.mobile_mo.session_id_parameter = false
-  initializer "mobile_mo.extend.action_controller" do |app|
-    ActiveSupport.on_load :action_controller do
-      include MobileMo::ActionController::Base
-      include MobileMo::ActionController::Haml if defined?(Haml)
-      include MobileMo::ActionController::SessionIdParameter if app.config.mobile_mo.session_id_parameter
+module MobileMo
+  class Railtie < Rails::Railtie
+    config.mobile_mo = ActiveSupport::OrderedOptions.new
+    config.mobile_mo.session_id_parameter = false
+    initializer "mobile_mo.extend.action_controller" do |app|
+      ActiveSupport.on_load :action_controller do
+        include MobileMo::ActionController::Base
+        include MobileMo::ActionController::Haml if defined?(Haml)
+        include MobileMo::ActionController::SessionIdParameter if app.config.mobile_mo.session_id_parameter
+      end
     end
-  end
-  initializer "mobile_mo.include.request" do
-    ActionDispatch::Request.send :include, MobileMo::Request
+    initializer "mobile_mo.include.request" do
+      ActionDispatch::Request.send :include, MobileMo::Request
+    end
   end
 end
