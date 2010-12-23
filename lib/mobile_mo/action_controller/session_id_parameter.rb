a24b5bb0f0066ab9a222f20d66db4747bfcc9252
Paul McMahon <paul@mobalean.com>
2010-12-23 06:34:26 -0800

    Rename module for session id parameter

---
+++ lib/mobile_mo/action_controller/session_id_parameter.rb
@@ -0,0 +1,27 @@
+module MobileMo
+  module ActionController
+    module SessionIdParameter
+      def self.included(klass)
+        klass.before_filter :set_session_id_parameter
+      end
+
+      protected # the following methods are available within the controller
+
+      def device_needs_session_param_in_url?
+        is_mobile_device? && !request.cookies? && session
+      end
+
+      def set_session_id_parameter
+        key = ::Rails.application.config.session_options[:key]
+        if device_needs_session_param_in_url?
+          session[:_dummy_param_to_force_session_init] = nil
+          logger.debug("adding session param '#{key}' to default_url_options")
+          default_url_options[key] = request.session_options[:id]
+        else
+          # workaround for issue in rails 3.0.3: default_url_options aren't cleared
+          default_url_options.delete(key)
+        end
+      end
+    end
+  end
+end
--- lib/mobile_mo/railtie.rb
+++ lib/mobile_mo/railtie.rb
@@ -4,7 +4,7 @@ class MobileMo::Railtie < Rails::Railtie
   initializer "mobile_mo.extend.action_controller" do |app|
     ActiveSupport.on_load :action_controller do
       include MobileMo::ActionController::Base
-      include MobileMo::SessionIdParameter if app.config.mobile_mo.session_id_parameter
+      include MobileMo::ActionController::SessionIdParameter if app.config.mobile_mo.session_id_parameter
     end
   end
   initializer "mobile_mo.include.request" do
