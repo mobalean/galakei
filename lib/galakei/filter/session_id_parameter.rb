e11938c5a0a026d00fb3c007f8247ed6c9346ed2
Michael Reinsch <michael@mobalean.com>
2011-03-06 03:12:20 +0900

    updated session ID handling so it'll work even if the session is reset (for instance by Devise upon login)

--- lib/galakei/filter/session_id_parameter.rb
+++ lib/galakei/filter/session_id_parameter.rb
@@ -6,11 +6,20 @@ class Galakei::Filter::SessionIdParameter < Galakei::Filter::Base
   def filter
     key = ::Rails.application.config.session_options[:key]
     if device_needs_session_param_in_url?
-      session[:_dummy_param_to_force_session_init] = nil
-      logger.debug("adding session param '#{key}' to default_url_options")
-      default_url_options[key] = request.session_options[:id]
+      session_opts = env[ActionDispatch::Session::AbstractStore::ENV_SESSION_OPTIONS_KEY]
+      # if we don't have a session ID yet, create one
+      if session_opts[:id].blank?
+        # make sure to reset any active record session store,
+        # we'll have to create a new one for the new session
+        env[ActiveRecord::SessionStore::SESSION_RECORD_KEY] = nil
+        # create a new session ID
+        session_opts[:id] = ActiveSupport::SecureRandom.hex(8)
+      end
+      sid = session_opts[:id]
+      logger.debug("Galakei: adding session param '#{key}' to default_url_options")
+      default_url_options[key] = sid
     else
-      # workaround for issue in rails 3.0.3: default_url_options aren't cleared
+      # default_url_options aren't cleared, so we need to clear them
       default_url_options.delete(key)
     end
   end
@@ -24,6 +33,5 @@ class Galakei::Filter::SessionIdParameter < Galakei::Filter::Base
   def default_url_options
     controller.send :default_url_options
   end
-
 
 end
