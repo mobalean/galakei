aa9769095f00fa66bd481740181bd8d09c158afe
Paul McMahon <paul@mobalean.com>
2011-04-04 17:50:29 +0900

    Add test for haml

---
+++ spec/acceptance/haml_spec.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+class HamlController < ApplicationController
+end
+
+feature 'haml' do
+  %w[softbank au docomo].each do |s|
+    scenario "for #{s}", :driver => s.to_sym do
+      pending 'until switch from external docomo_css' if s == 'docomo'
+      visit '/haml'
+      page.body.should include("<?xml version='1.0' encoding='utf-8' ?>")
+      page.body.should include('<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">') 
+      page.body.should include('<br />')
+      page.should have_css('br')
+    end
+  end
+end
