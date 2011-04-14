185a6114ee51db212629c0fcf964bf66aed7c1ab
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-04-14 12:01:01 +0900

    Fix DEPRECATION WARNING

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
  Rails.application.routes.draw do |map|
-   match 'galakei/spacer/:color' => 'galakei/spacer#create', :format => :gif
+   match 'galakei/spacer/:color' => 'galakei/spacer#create', :defaults => { :format => :gif }
  end
