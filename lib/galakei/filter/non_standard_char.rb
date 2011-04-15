2068193418d6f9144a4a6ac145b41cd5084e34c0
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-04-15 13:35:57 +0900

    Update NonStandardChar filter for Nakaguro

--- lib/galakei/filter/non_standard_char.rb
+++ lib/galakei/filter/non_standard_char.rb
@@ -10,6 +10,14 @@ class Galakei::Filter::NonStandardChar < Galakei::Filter::Base
   end
 
   def filter
-    response.body = response.body.gsub(/・/,'&middot;')
+    body = response.body
+    full_dot = "\u30FB"
+    harf_dot = "\uFF65"
+    body.gsub!("&middot;", full_dot)
+    body.gsub!("\u00B7",   harf_dot)
+    body.gsub!("&#xFF65;", harf_dot)
+    body.gsub!("&#x30FB;", full_dot)
+    body.gsub!("&sdot;",   harf_dot)
+    response.body = body
   end
 end
--- spec/acceptance/non_standard_char_spec.rb
+++ spec/acceptance/non_standard_char_spec.rb
@@ -2,16 +2,52 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 class NonStandardCharController < ApplicationController
-  def index
-    render :inline => '・'
+  def middot
+    render :inline => '&middot;'
+  end
+
+  def latin
+    render :inline => "\u00B7"
+  end
+
+  def harf_entity
+    render :inline => "&#xFF65;"
+  end
+
+  def full_entity
+    render :inline => "&#x30FB;"
+  end
+
+  def sdot
+    render :inline => "&sdot;"
   end
 end
 
 feature 'nakaguro' do
   %w[softbank au docomo].each do |s|
-    scenario "convert nakaguro to &middot;", :driver => s.to_sym  do
-      visit '/non_standard_char'
-      page.body.should == '&middot;'
+    scenario "convert &middot to \u30FB for #{s}", :driver => s.to_sym  do
+      visit '/non_standard_char/middot'
+      page.body.should == "\u30FB"
+    end
+
+    scenario "convert latin dot(\u00B7) to \uFF65 for #{s}", :driver => s.to_sym  do
+      visit '/non_standard_char/latin'
+      page.body.should == "\uFF65"
+    end
+
+    scenario "convert entity of harf-size dot to \uFF65 for #{s}", :driver => s.to_sym  do
+      visit '/non_standard_char/harf_entity'
+      page.body.should == "\uFF65"
+    end
+
+    scenario "convert entity of full-size dot to \u30FB for #{s}", :driver => s.to_sym  do
+      visit '/non_standard_char/full_entity'
+      page.body.should == "\u30FB"
+    end
+
+    scenario "convert &sdot; to \uFF65 for #{s}", :driver => s.to_sym  do
+      visit '/non_standard_char/sdot'
+      page.body.should == "\uFF65"
     end
   end
 end
