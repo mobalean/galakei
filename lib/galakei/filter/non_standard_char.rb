1eff6c55ee24b0b601131b62b3cace565b261610
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2011-04-15 19:36:42 +0900

    Fix typo

--- lib/galakei/filter/non_standard_char.rb
+++ lib/galakei/filter/non_standard_char.rb
@@ -12,12 +12,12 @@ class Galakei::Filter::NonStandardChar < Galakei::Filter::Base
   def filter
     body = response.body
     full_dot = "\u30FB"
-    harf_dot = "\uFF65"
+    half_dot = "\uFF65"
     body.gsub!("&middot;", full_dot) if request.docomo?
-    body.gsub!("\u00B7",   harf_dot) unless request.softbank?
-    body.gsub!("&#xFF65;", harf_dot) if request.au?
+    body.gsub!("\u00B7",   half_dot) unless request.softbank?
+    body.gsub!("&#xFF65;", half_dot) if request.au?
     body.gsub!("&#x30FB;", full_dot) if request.au?
-    body.gsub!("&sdot;",   harf_dot)
+    body.gsub!("&sdot;",   half_dot)
     response.body = body
   end
 end
