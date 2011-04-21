06d4a66417d953f1c35e63f494a17c49cf680994
Junya Ishihara <junya@champierre.com>
2011-04-21 17:01:12 +0900

    Convert Shift JIS params to UTF-8 on au SSL

--- lib/galakei/filter/recode.rb
+++ lib/galakei/filter/recode.rb
@@ -1,8 +1,9 @@
 # Takes care of recoding pages to Shift-JIS for some handsets when required
+require 'nkf'
 class Galakei::Filter::Recode < Galakei::Filter::Base
   def self.inject(klass)
     this_class = self
-    klass.after_filter self, :if => lambda {|c| this_class.condition?(c) }
+    klass.around_filter self, :if => lambda {|c| this_class.condition?(c) }
   end
 
   def condition?
@@ -10,8 +11,30 @@ class Galakei::Filter::Recode < Galakei::Filter::Base
   end
 
   def filter
+    deep_apply(controller.params) do |param|
+      NKF.nkf('-Sw', param)
+    end
+    yield
     doc = Nokogiri::HTML(response.body)
     response.body = doc.serialize(:encoding => 'Shift_JIS')
     response.charset = "Shift_JIS"
+  end
+
+  private
+  def deep_apply(obj, &proc)
+    case obj
+    when Hash
+      obj.each_pair do |key, value|
+        obj[key] = deep_apply(value, &proc)
+      end
+    when Array
+      obj.collect!{|value| deep_apply(value, &proc)}
+    when String
+      obj = obj.to_param if obj.respond_to?(:to_param)
+      proc.call(obj)
+    else
+      # NilClass, TrueClass, FalseClass, Tempfile, StringIO, etc...
+      return obj
+    end
   end
 end
--- spec/acceptance/recode_spec.rb
+++ spec/acceptance/recode_spec.rb
@@ -2,8 +2,25 @@
 require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 class RecodeController < ApplicationController
+  class << self
+    attr_accessor :string
+  end
+
   def index
     render :layout => true, :inline => "Test"
+  end
+
+  def new
+    render :layout => true, :inline => <<-EOD
+    <form action="/recode/create" method="post">
+    <input id="string" name="string" type="text">
+    <input name="commit" type="submit" value="Submit">
+    </form>
+    EOD
+  end
+
+  def create
+    self.class.string = params[:string]
   end
 end
 
@@ -11,5 +28,19 @@ feature 'recode' do
   scenario 'au SSL', :driver => :au do
     visit 'https://www.example.com/recode/index'
     page.find(:xpath, '//head/meta')['content'].should == "text/html; charset=Shift_JIS"
+  end
+
+  scenario 'post request with Shift JIS data on au SSL', :driver => :au do
+    visit 'https://www.example.com/recode/new'
+    fill_in 'string', :with => "\x82\xA0"
+    click_button "Submit"
+    RecodeController.string.should == "あ"
+  end
+
+  scenario 'post request with UTF-8 data on au SSL', :driver => :au do
+    visit 'https://www.example.com/recode/new'
+    fill_in 'string', :with => "あ"
+    click_button "Submit"
+    RecodeController.string.should_not == "あ"
   end
 end
