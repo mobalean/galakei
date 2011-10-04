ad9f77570fc0d23c1f33522a45971f1277aea6ff
Michael Reinsch <michael@mobalean.com>
2011-10-04 11:10:49 +0900

    define filter outside the action controller

--- lib/galakei/railtie.rb
+++ lib/galakei/railtie.rb
@@ -2,10 +2,10 @@ module Galakei
   class Railtie < ::Rails::Railtie
     config.galakei = ActiveSupport::OrderedOptions.new
     initializer "galakei.extend.action_controller" do |app|
+      filters = %w[Views ContentType Recode NonStandardChar]
+      filters << :Haml if defined?(Haml)
       ActiveSupport.on_load :action_controller do
         include Galakei::HelperMethods
-        filters = %w[Views ContentType Recode NonStandardChar]
-        filters << :Haml if defined?(Haml)
         filters.each {|f| Galakei::Filter.const_get(f).inject(self) }
       end
       ActiveSupport.on_load :action_view do
