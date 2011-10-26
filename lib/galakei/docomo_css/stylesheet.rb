mobalean/galakei

commit 3a63728d1c9c6a384eb27915390c10bd29a90f86

Improve documentation

lib/galakei/docomo_css/stylesheet.rb
@@ -1,7 +1,7 @@
 require "css_parser"
 require 'nokogiri'
 
-class Galakei::DocomoCss::Stylesheet
+module Galakei::DocomoCss::Stylesheet # :nodoc: all
   def initialize(parsed_stylesheet)
     @parsed_stylesheet = parsed_stylesheet
   end
@@ -103,7 +103,7 @@ EOD
     end
   end
 
-  class BackGroundAdapter < GenericAdapter
+  class BackGroundAdapter < GenericAdapter 
     def self.apply?(property)
       property == 'background-color'
     end

.gitignore
@@ -6,3 +6,5 @@ log
 *~
 TAGS
 tmp
+doc/*
+html/*

README.rdoc
@@ -1,0 +1,29 @@
+= Galakei: Japanese feature phone support
+
+{Japanese feature phones}[http://www.mobalean.com/en/keitai_web_technology_guide] (a.k.a., keitai, galakei) have a number of restrictions over normal web browsers.  This library adds support for them.
+
+== Goals
+
+* Provide support for 3G handsets from the major 3 carriers in Japan (docomo, au, SoftBank)
+* Avoid modifying Rails internals as much as possible
+
+== Functionality
+
+=== The Basics
+
+* {Convert to and from Shift-JIS when necessary}[rdoc-ref:Galakei::Filter::Recode]
+* {Correct Content-Type}[rdoc-ref:Galakei::Filter::ContentType]
+* {HTML Email}[rdoc-ref:Galakei::Email]
+* {Inlining of CSS}[rdoc-ref:Galakei::DocomoCss]
+* {Input Mode Switching}[rdoc-ref:Galakei::InputMode]
+* {Maintaining Session}[rdoc-ref:Galakei::SessionIdParameter]
+* {Substitution of Unsupported Characters}[rdoc-ref:Galakei::Filter::NonStandardChar]
+* {Unicode compatible Emoji}[rdoc-ref:Galakei::HelperMethods#emoji_table]
+* {Zenkaku to Hankaku conversion}[rdoc-ref:Galakei::Filter::Hankaku]
+* {galakei specific views}[rdoc-ref:Galakei::Filter::Views]
+* {haml support}[rdoc-ref:Galakei::Filter::Haml]
+
+== Thanks
+
+* To {jpmobile}[https://github.com/jpmobile/jpmobile] for offering the most mature Rails plugin for Rails
+* To {docomo_css}[https://github.com/milk1000cc/docomo_css] for providing the inspiration for galakei/docomo_css

Rakefile
@@ -1,6 +1,12 @@
 require 'bundler'
 require 'rspec/core/rake_task'
+require 'rdoc/task'
 Bundler::GemHelper.install_tasks
 
 task :default => :spec
 RSpec::Core::RakeTask.new
+
+RDoc::Task.new :rdoc do |rdoc|
+  rdoc.main = "README.doc"
+  rdoc.rdoc_files.include("README.rdoc", "lib/**/*.rb")
+end

lib/galakei.rb
@@ -9,7 +9,7 @@ module Galakei
   autoload :EmojiTable, "galakei/emoji_table"
   autoload :SessionIdParameterInForm, "galakei/session_id_parameter_in_form"
   autoload :Spacer, "galakei/spacer"
-  module Filter
+  module Filter # :nodoc:
     autoload :Base, "galakei/filter/base"
     autoload :ContentType, "galakei/filter/content_type"
     autoload :Haml, "galakei/filter/haml"

lib/galakei/docomo_css.rb
@@ -1,4 +1,36 @@
-module Galakei::DocomoCss
+=begin
+= Inlining Styles
+
+== Basic Inlining
+
+Old docomo handsets {don't support external stylesheets}[http://www.keitai-dev.net/CSS]. Additionally, only very limited CSS is supported. galakei/docomo_css automatically inlines CSS and manipulates markup to overcome these limitations.
+
+ # css file
+ h1 { color: red; background-color: blue}
+
+ # source html
+ <h1>Foo</h1>
+
+ # outputted html
+ <div style="background-color: blue;"><h1><span style="color:red;">Foo</span></h1>/div>
+
+== Borders
+
+Furthermore, the css border property is supported through the "spacer.gif" technique.
+
+ # css file
+ div { border-bottom: 5px solid #000000; }
+
+ # source html
+ <div>Foo</div>
+
+ # outputted html
+ <div>Foo</div>
+ <img src="/galakei/spacer/000000" width="100%" height="5">
+
+This will generate a 1px by 1px image, emulating the effect of borders on old docomo handsets.
+=end
+module Galakei::DocomoCss # :nodoc:
   autoload :Stylesheet, "galakei/docomo_css/stylesheet"
   autoload :InlineStylesheet, "galakei/docomo_css/inline_stylesheet"
 end

lib/galakei/docomo_css/inline_stylesheet.rb
@@ -2,7 +2,7 @@
 require 'css_parser'
 require 'nokogiri'
 
-class Galakei::DocomoCss::InlineStylesheet
+class Galakei::DocomoCss::InlineStylesheet # :nodoc: all
   def self.filter(controller)
     return unless controller.request.imode_browser_1_0?
     doc = Nokogiri::HTML(controller.response.body)

lib/galakei/docomo_css/railtie.rb
@@ -1,4 +1,4 @@
-module Galakei
+module Galakei # :nodoc: all
   module DocomoCss
     class Railtie < Rails::Railtie
       initializer "galakei.docomo_css" do |app|

commit 4a000d583155d3dc78699eee103cb917eeded858

Fix for bug introduced in 3a63728d

lib/galakei/docomo_css/stylesheet.rb
@@ -1,7 +1,7 @@
 require "css_parser"
 require 'nokogiri'
 
-module Galakei::DocomoCss::Stylesheet # :nodoc: all
+class Galakei::DocomoCss::Stylesheet # :nodoc: all
   def initialize(parsed_stylesheet)
     @parsed_stylesheet = parsed_stylesheet
   end