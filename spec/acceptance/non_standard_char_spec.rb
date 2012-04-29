49679b67a34e224e92514fc16b63e591ea54e627
Michael Reinsch <michael@mobalean.com>
2012-04-29 18:05:10 +0900

    simplified require

--- spec/acceptance/non_standard_char_spec.rb
+++ spec/acceptance/non_standard_char_spec.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+require 'acceptance/acceptance_helper'
 
 class NonStandardCharController < ApplicationController
   def self.character(name, value, options = {})
